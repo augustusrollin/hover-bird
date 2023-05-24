1111632c04f81a32cef47fcd548479b9e3037e10
Khundakjie <132590445+Khundakjie@users.noreply.github.com>
2023-05-24 13:10:03 -0400

    a

--- GameRunner.java
+++ GameRunner.java
@@ -64,10 +64,10 @@ public class GameRunner {
 
     public ArrayList<Render> getRenders() {
         ArrayList<Render> renders = new ArrayList<Render>();
-        renders.add(new Render(0, 0, "images/monkeybackground2.png"));
+        renders.add(new Render(0, 0, "images/FuturisticSpaceBackground.png"));
         for (Obstacles pipe : pipes)
             renders.add(pipe.getRender());
-        renders.add(new Render(0, 0, "images/foreground.png"));
+        renders.add(new Render(0, 720, "images/thin_background.png"));
         renders.add(character.getRender());
         return renders;
     }
