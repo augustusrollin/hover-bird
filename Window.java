c15dca301d66d105a25bf3f9fee62ee4fcecbe26
augustusrollin <pikachu@reactsoftwareinc.com>
2023-06-05 20:35:02 -0400

    window comments

--- Window.java
+++ Window.java
@@ -14,36 +14,45 @@ import javax.sound.sampled.Clip;
 
 public class Window {
 
-    static Properties prop;
+    static Properties prop; // Hold properties from the configuration file
 
-    public static int WIDTH;
-    public static int HEIGHT;
+    public static int WIDTH; // Store the width of the window
+    public static int HEIGHT; // Store the height of the window
 
     public static void main(String[] args)
             throws AWTException, UnsupportedAudioFileException, IOException, LineUnavailableException {
+        
         try (InputStream input = new FileInputStream("config.properties")) {
             prop = new Properties();
 
-            // load a properties file
+            // Load properties from the configuration file
             prop.load(input);
+
+            // Retrieve and assign the width and height from the properties
             WIDTH = Integer.parseInt(prop.getProperty("window.WIDTH"));
             HEIGHT = Integer.parseInt(prop.getProperty("window.HEIGHT"));
         } catch (IOException ex) {
-            ex.printStackTrace();
+            ex.printStackTrace(); // Print the stack trace if there is an exception while loading properties
         }
-        JFrame frame = new JFrame();
-        frame.setUndecorated(true);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setLocation(-10, 0);
+        
+        JFrame frame = new JFrame(); // Create a new JFrame object
 
-        Keyboard keyboard = Keyboard.getInstance();
-        frame.addKeyListener(keyboard);
+        // Configure the window properties
+        frame.setUndecorated(true); // Remove window decorations (title bar, buttons, etc.)
+        frame.setVisible(true); // Make the window visible
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the application when the window is closed
+        frame.setLocationRelativeTo(null); // Center the window on the screen
+        frame.setLocation(-10, 0); // Set the initial window position
 
-        GamePanel panel = new GamePanel();
+        Keyboard keyboard = Keyboard.getInstance(); // Get an instance of the Keyboard class
+        frame.addKeyListener(keyboard); // Add the Keyboard object as a key listener to the frame
+
+        GamePanel panel = new GamePanel(); // Create a new GamePanel object
+
+        // Add the GamePanel object to the frame
         frame.add(panel);
-        frame.setResizable(false);
-        frame.setSize(WIDTH, HEIGHT);
+
+        frame.setResizable(false); // Disable window resizing
+        frame.setSize(WIDTH, HEIGHT); // Set the size of the window based on the WIDTH and HEIGHT variables
     }
 }
