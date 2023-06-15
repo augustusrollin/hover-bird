145069d1b6a05e4ec10371defeffac083f0909b1
augustusrollin <pikachu@reactsoftwareinc.com>
2023-06-14 21:07:17 -0400

    different boost colors for each mode

--- GamePanel.java
+++ GamePanel.java
@@ -35,7 +35,7 @@ public class GamePanel extends JPanel implements Runnable {
             }
 
             g2D.setColor(Color.WHITE);
-            position = new Point(650,300);
+            position = new Point(650,250);
             int spacing = 50;
             int fontSize = 40;
 
@@ -49,26 +49,38 @@ public class GamePanel extends JPanel implements Runnable {
                 g2D.drawString("H for Hell Mode", position.x, position.y + 3*spacing);
                 g2D.drawString("O for Ocean Mode", position.x, position.y + 4*spacing);
                 g2D.drawString("P for Panda Mode", position.x, position.y + 5*spacing);
+                g2D.drawString("A for Arctic Mode", position.x, position.y + 6*spacing);
                 // NOTE: Leave a space inbetween the modes an levels
                 //dificulties
-                g2D.drawString("1 for easy difficulty", position.x, position.y + 7*spacing);
-                g2D.drawString("2 for medium difficulty", position.x, position.y + 8*spacing);
-                g2D.drawString("3 for hard difficulty", position.x, position.y + 9*spacing);
-                g2D.drawString("4 for impossible difficulty", position.x, position.y + 10*spacing);
+                g2D.drawString("1 for easy difficulty", position.x, position.y + 8*spacing);
+                g2D.drawString("2 for medium difficulty", position.x, position.y + 9*spacing);
+                g2D.drawString("3 for hard difficulty", position.x, position.y + 10*spacing);
+                g2D.drawString("4 for impossible difficulty", position.x, position.y + 11*spacing);
             } else {
                 // Display the score and rocketfuel during gameplay
                 g2D.setFont(new Font("Futurism", Font.PLAIN, fontSize));
                 g2D.drawString(Integer.toString(gameRunner.score), 30, 60);
                 if (!GameRunner.gameover) {
                     //g2D.drawString("Rocket Fuel: " + Integer.toString(Character.rocketFuel), 1150, 60);
-                    Color darkRed = new Color(80, 0 ,0);
-                    Color purple = new Color(174, 55, 255);
+                    Color darkRed = new Color(80, 0 ,0); // hell
+                    Color purple = new Color(174, 55, 255); // future
+                    Color pink = new Color(255, 84, 220); // rainbow
+                    Color orange = new Color(255, 149, 102); // ocean
+                    Color black = new Color(0, 0, 0); // panda
+                    Color cyan = new Color(140, 226, 255); // ocean
                     
-                    // TODO: Add the other modes here
                     if(Mode.characterImage.equals("hellSpaceshipWorking")){
                         g2D.setColor(darkRed);
-                    } else {
+                    } else if(Mode.characterImage.equals("SpaceshipTestWorking")){
                         g2D.setColor(purple);
+                    } else if(Mode.characterImage.equals("UnicornNew")){
+                        g2D.setColor(pink);
+                    } else if(Mode.characterImage.equals("shark")){
+                        g2D.setColor(orange);
+                    } else if(Mode.characterImage.equals("panda")){
+                        g2D.setColor(black);
+                    } else{
+                        g2D.setColor(cyan);
                     }
                     g2D.fillRect(1150,10, Character.rocketFuel/4, 60); // rocket fuel drawing
                     g2D.setColor(Color.white);
